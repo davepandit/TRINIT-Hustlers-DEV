687959153ce160c7f3bd382d8f934eb63376bbe7
Debajyoti Pandit <codeninja0812@gmail.com>
2024-03-09 14:08:07 +0530

    Some configs made

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
                 <h2 className='font-bold text-3xl'><Link to={'/'}>Test Formatter📒</Link></h2>
             </div>
             <div className='flex gap-x-4'>
-                <button className='bg-black text-white rounded-3xl px-4 py-2 hover:bg-white hover:text-black transition duration-300 ease-in-out'>SignUp</button>
-                <button className='bg-black text-white rounded-3xl px-4 py-2 hover:bg-white hover:text-black transition duration-300 ease-in-out'>Login</button>
+                <button className='bg-black text-white rounded-3xl px-4 py-2 hover:bg-white hover:text-black transition duration-300 ease-in-out'><Link to={'/signup'}>Signup</Link></button>
+                <button className='bg-black text-white rounded-3xl px-4 py-2 hover:bg-white hover:text-black transition duration-300 ease-in-out'><Link to={'/login'}>Login</Link></button>
             </div>
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,9 @@ import PreQues from '../../components/premadequestions/PreQues'
 const Home = () => {
   return (
     <>
-        <div class="relative inline-block mt-5 ml-7 ">
-            <span class="relative z-10 p-4 font-bold text-lg">Already Designed For You 😁</span>
-            <span class="absolute inset-0 bg-blue-100 transform -skew-x-6 z-0"></span>
+        <div className="relative inline-block mt-5 ml-7 ">
+            <span className="relative z-10 p-4 font-bold text-lg">Already Designed For You 😁</span>
+            <span className="absolute inset-0 bg-blue-100 transform -skew-x-6 z-0"></span>
         </div>
         <div className='mt-5 grid grid-cols-3 gap-4'>
             <PreQues /> 
@@ -15,6 +15,13 @@ const Home = () => {
             <PreQues /> 
             <PreQues /> 
             <PreQues /> 
+        </div>
+        <div className="relative inline-block mt-7 ml-7 ">
+          <span class="relative z-10 p-4 font-bold text-lg">Bored with the premixes, Create your own🔥</span>
+          <span class="absolute inset-0 bg-blue-100 transform -skew-x-6 z-0"></span>
+        </div>
+        <div>
+          
         </div>
     </>
   )
