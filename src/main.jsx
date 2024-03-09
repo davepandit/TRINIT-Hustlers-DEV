1b3388e5c622c5896a6c24f7c5ad763aa5dc69dc
Debajyoti Pandit <codeninja0812@gmail.com>
2024-03-09 14:47:23 +0530

    First user Signup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
+import FirebaseContextProvider from './context/Firebase.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <FirebaseContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </FirebaseContextProvider>
   </React.StrictMode>,
 )
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,16 +1,37 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useFirebase } from '../../context/Firebase'
 
 function Signup() {
+    const [email , setEmail] = useState("")
+    const [password , setpassword] = useState("")
+
+    //establishing a link between the component and the context
+
+    const firebase = useFirebase()
+
+    //handleSubmit
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        await firebase.SignUpUserWithEmailAndPassword(email , password ).then((result)=>(console.log("usersignup done")))
+        
+
+
+    }
    
     return (
         <div className=' flex justify-center items-center h-screen'>
-            <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
+            <form className=' bg-gray-800 px-10 py-10 rounded-xl '
+            onSubmit={handleSubmit}
+            >
                 <div className="">
                     <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
                 </div>
                 <div>
                     <input type="email"
                         name='email'
+                        value={email}
+                        onChange={(e)=>(setEmail(e.target.value))}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
@@ -18,6 +39,8 @@ function Signup() {
                 <div>
                     <input
                         type="password"
+                        value={password}
+                        onChange={(e)=>(setpassword(e.target.value))}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
@@ -31,7 +54,7 @@ function Signup() {
                 <div>
                     <h2 className='text-white'>Have an account <Link className=' text-red-500 font-bold' to={'/login'}>Login</Link></h2>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
